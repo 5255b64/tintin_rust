62deaf3ae5176915765b83b19410ef9cdbae70e9
Gx <475444812@qq.com>
2024-03-14 01:38:29 +0800

    Rust入门-第一课

---
+++ src/main.rs
@@ -0,0 +1,23 @@
+
+fn main() {
+    println!("a2Z:");
+    a2Z::print_a2Z();
+    println!("A2z:");
+    a2Z::z2A::print_A2z();
+}
+
+mod a2Z{
+    pub fn print_a2Z() {
+        for c in b'a'..=b'Z' {
+            println!("{}", c as char);
+        }
+    }
+
+    pub mod z2A{
+        pub fn print_A2z() {
+            for c in b'A'..=b'z' {
+                println!("{}", c as char);
+            }
+        }
+    }
+}
